5db43b134f90610c84552fc87acc64eaa64a9a5d
JavierDominguezGomez <jdomgo3@gmail.com>
2020-09-19 18:36:45 +0200

    First app version.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { GifFinderApp } from "./GifFinderApp";
+
+import "./index.css";
+
+const divRoot = document.querySelector("#root");
+
+ReactDOM.render(<GifFinderApp />, divRoot);
